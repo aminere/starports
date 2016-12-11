84194163b5882ee25a8f5e646eb6ed604779add1
Amine Rehioui <amine.reh@gmail.com>
2016-12-10 23:57:10 -0500

    Separate Game and Editor build scripts

--- BuildGame.bat
+++ BuildGame.bat
@@ -1,7 +1,4 @@
 
-echo Building Editor..
-call "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" "Game\build\vc\editor\EditorLauncher.sln" /m /p:Configuration=Release /v:normal
-
 echo Building Game..
 call "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" "Game\build\vc\game\GameLauncher.sln" /m /p:Configuration=Release /v:normal
 
