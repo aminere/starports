63b33b10258e12a8d0d596ddf39dd19bc4ca99e5
Amine Rehioui <amine@franticsoftware.com>
2020-12-09 16:43:26 -0500

    Update build scripts

--- BuildEditor.bat
+++ BuildEditor.bat
@@ -1,3 +1,9 @@
 
+@echo off
+
 echo Building Editor..
-call "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" "Game\build\vc\editor\EditorLauncher.sln" /m /p:Configuration=Release /v:normal
+
+set MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild.exe"
+
+call %MSBUILD% "Game\build\vc\editor\EditorLauncher.sln" /m /p:Configuration=Release /v:normal
+
--- BuildGame.bat
+++ BuildGame.bat
@@ -1,9 +1,13 @@
 
+@echo off
+
+set MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild.exe"
+
 echo Building Game..
-call "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" "Game\build\vc\game\GameLauncher.sln" /m /p:Configuration=Release /v:normal
+call %MSBUILD% "Game\build\vc\game\GameLauncher.sln" /m /p:Configuration=Release /v:normal
 
 echo Building FileSystemGenerator..
-call "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" "Shoot\tools\FileSystemGenerator\vc\FileSystemGenerator.sln" /m /p:Configuration=Release /v:normal
+call %MSBUILD% "Shoot\tools\FileSystemGenerator\vc\FileSystemGenerator.sln" /m /p:Configuration=Release /v:normal
 
 echo Building PC Assets..
 pushd Game
